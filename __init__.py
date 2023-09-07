aa6161a6017c7ddcd505ec184953ca45f525dbb0
thejonasmacedo <jonas.b.m@outlook.com>
2023-09-06 23:01:02 -0300

    added main.py | templates file

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-
-@app.route('/', methods=['GET'])
-def root():
-    return jsonify({'message': 'Hello World'})
---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/sign up')
+def sign_up():
+    return render_template('signup.html')
