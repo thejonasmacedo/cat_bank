21db0ebee69b783d20bfc9a9ad2fa2dc4f34ece1
thejonasmacedo <jonas.b.m@outlook.com>
2023-09-07 13:47:31 -0300

    fixed login request method and added static file

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='/static')
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return 'site main'
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'jonas' and password == '1234':
+            return 'Login bem-sucedido'
+        else:
+            return 'Login Falhou'
+
+    else:
+        return render_template('login.html')
+
+
+@app.route('/signup')
+def sign_up():
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/sign up')
-def sign_up():
-    return render_template('signup.html')
